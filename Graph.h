91744741387984b70bb08402749084a720626a66
Andrii <ramp8a799@gmail.com>
2021-11-21 13:20:15 +0200

    Improved cration of the praph and separated it individual functions

--- Graph.h
+++ Graph.h
@@ -4,44 +4,59 @@
 #include <ctime>
 #include <string>
 using namespace std;
-void createGraph(int n, string path) {
+vector<vector<int>> createGraph(int n, string path) {
+
 	vector<vector<int>> graph(n, vector<int>(n));
+	vector<vector<int>> visited(n, vector<int>(n));
+
 	vector<int> degrees(n);
+
 	srand(time(NULL));
-	ofstream fout;
-	fout.open(path);
-	if (!fout.is_open())
-		exit(1);
+
 	for(auto i = 0; i < n; i++) {
 		for(auto j = 0; j < n; j++) {
 
-			if (i == j) {
-				if (j < n - 1)
-					fout << graph[i][j] << " ";
-				else
-					fout << graph[i][j];
+			if (i == j)
 				continue;
-			}
 
-			//int fill_or_not = rand() % 2 + 1;
+			int fill_or_not = rand() % 2 + 1;
 
-			if(degrees[i]<10 && graph[j][i]==0) {
+			if(fill_or_not%2==0 && degrees[i]<10 && visited[j][i]!=-1) {
+				visited[i][j] = -1;
+				visited[j][i] = -1;
 				degrees[i]++;
 				graph[i][j] = rand() % 150 + 5;
 				graph[j][i] = graph[i][j];
+
 			}
+		}
+	}
+	
+	return graph;
+}
+
+void setGraphInFile(int n, string path, vector<vector<int>> graph) {
+
+	ofstream fout;
+	fout.open(path);
+
+	if (!fout.is_open())
+		exit(1);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
 			if (j < n - 1)
 				fout << graph[i][j] << " ";
 			else
 				fout << graph[i][j];
 		}
-		if(i<n-1)
+		if (i < n - 1)
 			fout << "\n";
 	}
 	fout.close();
 }
 
-vector<vector<int>> getGraph(int n, string path) {
+vector<vector<int>> getGraphFromFile(int n, string path) {
 	vector<vector<int>> graph(n,vector<int>(n));
 	ifstream fin;
 	string temp;
--- Lab4.cpp
+++ Lab4.cpp
@@ -5,8 +5,10 @@ int main()
 { 
     int n = 10;
     string path = "Graph.txt";
-    //createGraph(n, path);
-    vector<vector<int>> grapgh = getGraph(n, path);
+    //vector<vector<int>> grapgh = createGraph(n, path);
+    //setGraphInFile(n, path, grapgh);
+    vector<vector<int>> grapgh = getGraphFromFile(n, path);
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
